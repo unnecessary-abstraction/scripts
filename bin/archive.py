85b3c5f2a8abd8cbcd8abb30bf35a8e2af32081e
Paul Barker <paul@pbarker.dev>
2022-02-12 11:04:29 +0000

    archive: Improve workflow, switch to zstd
    Signed-off-by: Paul Barker <paul@pbarker.dev>

--- bin/archive.py
+++ bin/archive.py
@@ -4,21 +4,16 @@
 
 """
 This script is used to archive files for long-term storage. Given a source
-directory of files to be archived, it produces 3 outputs:
+directory of files to be archived, it produces:
 
-    1) `files`: A copy of the archived files in their original form, marked as
-    read-only to prevent accidental modification.
+    1) A signed checksum file which can be used to verify that data remains
+    intact and unmodified.
 
-    2) `checksums`: A signed checksum file for each set of archived files which can
-    be used to verify that the copy under `files` is intact and unmodified.
-
-    3) `tarballs`: A compressed and encrypted tarball for each set of archived
-    files, suitable for long term storage off-site or in the cloud.
+    2) A compressed and encrypted tarball suitable for long term storage
+    off-site or in the cloud.
 
 The following configuration keys are read from the YAML file
 `~/.config/archive.yml`:
-
-    archive_path: The destination path for archived content.
 
     keyid: The gpg key to be used for signing and encrypting.
 """
@@ -36,8 +31,9 @@ def usage():
     print(
         textwrap.dedent(
             f"""\
-            Usage: {sys.argv[0]} IDENT SOURCE
-                Archives files in SOURCE for long-term storage with the identifier IDENT.
+            Usage: {sys.argv[0]} SOURCE [NAME]
+                Archives files in SOURCE for long-term storage.
+                Store output in SOURCE.archive or NAME.archive if a name is given.
                 Configuration is read from `~/.config/archive.yml`.\
             """
         )
@@ -53,46 +49,44 @@ if len(sys.argv) >= 2 and sys.argv[1] in ("-h", "--help"):
     usage()
     sys.exit(0)
 
-if len(sys.argv) != 3:
+if len(sys.argv) not in (2, 3):
     usage()
     sys.exit(1)
 
-IDENT, SOURCE_PATH = sys.argv[1:]
+SOURCE_PATH = sys.argv[1]
+if len(sys.argv) == 3:
+    IDENT = sys.argv[2]
+else:
+    IDENT = SOURCE_PATH
 
 with open(os.path.expanduser("~/.config/archive.yml")) as config_file:
     config = yaml.safe_load(config_file)
-    ARCHIVE_PATH = os.path.expanduser(os.path.expanduser(config["archive_path"]))
     KEYID = config["keyid"]
 
-# Copy files to destination
-files_dest_path = os.path.join(ARCHIVE_PATH, "files", IDENT)
-os.makedirs(os.path.dirname(files_dest_path))
-shutil.copytree(SOURCE_PATH, files_dest_path)
-os.chdir(files_dest_path)
+# Set files are read-only
+out_dir = os.getcwd()
+os.chdir(SOURCE_PATH)
 files_list = []
 for dirpath, dirnames, fnames in os.walk("."):
     files_list += [os.path.normpath(os.path.join(dirpath, fname)) for fname in fnames]
     for name in fnames + dirnames:
         set_readonly(os.path.join(dirpath, name))
-set_readonly(".")
-print(f"Archived to: {files_dest_path}")
 
 # Create checksums file
-checksum_path = os.path.join(ARCHIVE_PATH, "checksums", f"{IDENT}.b2sum")
-os.makedirs(os.path.dirname(checksum_path))
+checksum_path = "B2SUMS"
 with tempfile.NamedTemporaryFile() as tmpf:
     subprocess.run(["b2sum", *files_list], stdout=tmpf, check=True)
     subprocess.run(
-        ["gpg", "-as", "--clearsign", "-u", KEYID, "-o", checksum_path, tmpf.name]
+        ["gpg", "-as", "--clearsign", "-u", KEYID, "-o", checksum_path, tmpf.name],
+        check=True
     )
 set_readonly(checksum_path)
-print(f"Checksums: {checksum_path}")
+set_readonly(".")
 
 # Create encrypted tarball
-tarball_path = os.path.join(ARCHIVE_PATH, "tarballs", f"{IDENT}.archive")
-os.makedirs(os.path.dirname(tarball_path))
-with tempfile.NamedTemporaryFile() as tmpf:
-    subprocess.run(["tar", "cJ", *files_list], stdout=tmpf, check=True)
-    subprocess.run(["gpg", "-e", "-r", KEYID, "-o", tarball_path, tmpf.name])
+tarball_path = os.path.join(out_dir, f"{IDENT}.archive")
+with tempfile.NamedTemporaryFile(dir=out_dir) as tmpf:
+    subprocess.run(["tar", "-c", "--zstd", "B2SUMS", *files_list], stdout=tmpf, check=True)
+    subprocess.run(["gpg", "-e", "-r", KEYID, "-o", tarball_path, tmpf.name], check=True)
 set_readonly(tarball_path)
-print(f"Encrypted tarball: {tarball_path}")
+print(f"Encrypted archive: {tarball_path}")
